ee362415da65deb7a8b8033e0acd907ce59d7aba
troikm <karentroiano2009@gmail.com>
2014-08-12 22:37:03 -0430

    Actualizando el repositorio para el proyecto2. Se incluye mi proyecto de redes de RMI

---
+++ Proyecto01/src/Test.java
@@ -0,0 +1,112 @@
+/**
+* @author Karen Troiano		09-10855
+* @author Luis Miranda		10-10463
+* @author Jose Montenegro	10-10469
+*
+* Test: Clase main (?)
+*/
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Test {
+	
+	public static void main(String args[]){
+
+		if(args.length != 4){
+			System.out.println("Uso: java Test <Archivo> <Quantum> <TiempoIO> <NumeroCPUs>");
+			System.exit(0);
+		}
+
+		int quantum = 0;
+		int IOTime = 0;
+		int numCPUs = 0;
+
+      try{
+			quantum = Integer.parseInt(args[1]);
+			IOTime = Integer.parseInt(args[2]);
+			numCPUs = Integer.parseInt(args[3]);
+		}catch(NumberFormatException e){
+			System.out.println("El quantum, tiempo de IO o numero de CPUs introducidos no son numeros");
+			System.exit(1);
+		}
+
+		if(!(quantum > 0 && IOTime > 0 && numCPUs > 0)){
+			System.out.println("Tiempo de quantum,IO o CPU invalidos");
+			System.exit(1);
+		}
+		
+		Ready nuevo = new Ready();
+		Colas waiting = new Colas();
+		Ready ready = new Ready();
+		Colas finished = new Colas();
+		nuevo.parse(args[0]);
+		Tick timer = new Tick(nuevo.size(),numCPUs+1);
+
+		System.out.println(nuevo.getQueue());
+
+      Queue<CPU> cpus = new LinkedList<CPU>();
+		Queue<Thread> cpThreads = new LinkedList<Thread>();
+		for(int i=0;i<numCPUs;++i){
+			CPU temp = new CPU(timer,ready,waiting,nuevo,finished,quantum);
+			Thread c = new Thread(temp,"CPU" + i);
+			cpus.add(temp);
+			cpThreads.add(c);
+			c.start();
+		}	
+
+		Thread es = new Thread(new ES(timer,waiting,ready,finished,IOTime),"ES");
+		es.start();
+		
+		while(finished.size() < timer.getMaxProc()) {
+		    timer.tick();
+		}
+
+		try{
+			for(Thread t : cpThreads){
+				t.join();
+			}
+			es.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("-------------- Tiempo final:" + timer.getTime() +" --------------");
+		System.out.println("Proceso\t|\tTiempo final");
+		System.out.println(Proceso.all);
+
+		for(CPU c : cpus){
+			c.printIdlePercentage();
+		}
+
+		System.out.println("Tiempo promedio de ejecucion: " + 
+		getAverageRunningTime(finished,timer));
+
+		System.out.println("Tiempo promedio de espera: " +
+		getAverageWaitTime(finished,timer));
+		
+	}
+
+	public static int getAverageRunningTime(Colas f, Tick t){
+		int totalTime = 0;
+
+		for(Proceso p : f.getQueue())
+		{
+			totalTime += (p.getFinishTime() - p.getArrivalTime());
+		}
+
+		return totalTime/t.getMaxProc();
+	}
+
+	public static int getAverageWaitTime(Colas f, Tick t){
+		int totalTime = 0;
+
+		for(Proceso p: f.getQueue())
+		{
+			totalTime += p.getWaitTime() - p.getArrivalTime();
+		}
+
+		return totalTime/t.getMaxProc();
+	}
+
+}
---
+++ Proyecto01/src/Tick.java
@@ -0,0 +1,122 @@
+/**
+* @author Karen Troiano		09-10855
+* @author Luis Miranda		10-10463
+* @author Jose Montenegro	10-10469
+*
+* Clase Tick: Representa al monitor de los ticks
+*
+* @param time: Tiempo que lleva el manejador
+*/
+
+import java.util.Scanner;
+
+public class Tick{
+  
+  private int MAX_DEVICES;
+  private int time;
+  private boolean ok_to_tick;
+  private boolean ok_to_run;
+  private int finished;
+  private int numProc;
+  private boolean ok_to_while;
+    
+	/**
+	 * Constructor del Tick
+	 * @return Objeto Tick. Inicializa el thread del reloj
+	 **/
+	public Tick(int n, int c){
+		time = 0;
+		ok_to_tick = false;
+		ok_to_run = true;
+		MAX_DEVICES = c;
+		finished = MAX_DEVICES;
+		numProc = n;
+		ok_to_while = true;
+	}
+
+	public boolean getEnd(){
+		return ok_to_while;
+	}
+    
+    
+	/**
+	 * getTime: Obtiene el tiempo del manejador
+	 * @return Tiempo actual
+	 **/
+	public synchronized int getTime(){
+		return time;
+	}
+
+	/**
+	 * Tick: Hace el tick
+	 **/
+	public synchronized void tick(){
+		while(!ok_to_tick){
+			try{
+				wait();
+			}catch(IllegalMonitorStateException | InterruptedException e){
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+
+		time++;
+		ok_to_run = true;
+		ok_to_tick = false;
+		finished = MAX_DEVICES;
+		
+		if ( ((time % 5) == 0) ){
+			System.out.println("-------------- Tiempo " + time +" --------------");
+			System.out.println("Proceso\t|\tEstado");
+			System.out.println(Proceso.all);
+			System.out.println("Press enter to continue...");
+			Scanner keyboard = new Scanner(System.in);
+			keyboard.nextLine();
+		}
+		notifyAll();
+	}
+	/**
+	 * startJob: Inicializa un manejador
+	 **/
+	public synchronized void startJob(){
+		while(!ok_to_run){
+			try{
+				wait();
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * endJob: Termina un manejador
+	 **/
+	public synchronized void endJob(int i, Colas owari){
+		finished--;
+		
+		if(finished == 0)
+		{
+			ok_to_tick = true;
+			ok_to_run = false;
+			ok_to_while = owari.size() < this.getMaxProc();
+			
+			notifyAll();
+		}else{
+			try{
+				wait();
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public int getMaxProc(){
+		return numProc;
+	}
+
+	public synchronized void  endAll(){
+		finished = 0;
+		notifyAll();
+	}
+
+}
