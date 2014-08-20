230fffe4f801b91baf50dd4c89785b83811acd86
Jose Montenegro <josmontenegro2012@gmail.com>
2014-08-20 10:08:08 -0430

    Servidor y DNS

---
+++ Proyecto02/src/DNS.java
@@ -0,0 +1,97 @@
+//Servidor DNS
+
+import java.net.*;
+import java.io.*;
+
+public class DNS implements Runnable{
+
+	private static final int BUFFER_SIZE = 256;
+
+	private String ipPrincipal;
+	private Boolean failed;
+	private DatagramSocket socket;
+
+	public DNS(int port){
+		try{
+			socket = new DatagramSocket(port);
+			ipPrincipal = null;
+			failed = true;
+		}catch (SocketException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void run(){
+		while(true){
+			byte[] buf = new byte[BUFFER_SIZE];
+			DatagramPacket p = new DatagramPacket(buf,buf.length);
+
+			try{
+				System.out.println("Listening");
+				socket.receive(p);
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+
+			System.out.println("Message received");
+			String received = new String(p.getData(),0,p.getLength());
+			String response;
+			DatagramPacket resPacket;
+
+			if(received.startsWith("WHO")){
+				respondIP(p);
+			}else if(received.startsWith("FAILED")){
+				failed = true;
+				ipPrincipal = null;
+				respondIP(p);
+			}else if(received.startsWith("SERVER")){
+				respondCoord(p);
+			}
+		}
+	}
+
+	private void respondIP(DatagramPacket p){
+		DatagramPacket resPacket;
+		byte[] buf = new byte[BUFFER_SIZE];
+
+		if(ipPrincipal == null){
+			String response = "NOTHING";
+			buf = response.getBytes();
+		}else{
+			buf = ipPrincipal.getBytes();
+		}
+		resPacket = new DatagramPacket(buf,buf.length,p.getAddress(),p.getPort());
+
+		try{
+			socket.send(resPacket);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	private void respondCoord(DatagramPacket p){
+		DatagramPacket resPacket;
+		byte[] buf = new byte[BUFFER_SIZE];
+		String response;
+
+		if(failed){
+			response = "COORD";
+			ipPrincipal = p.getAddress().toString();
+			failed = false;
+		}else{
+			response = "OK";
+		}
+		buf = response.getBytes();
+		resPacket = new DatagramPacket(buf,buf.length,p.getAddress(),p.getPort());
+
+		try{
+			socket.send(resPacket);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]){
+		new Thread(new DNS(1111)).start();
+	}
+}									
--- Proyecto02/src/Servidor.java
+++ Proyecto02/src/Servidor.java
@@ -7,10 +7,58 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.net.*;
+import java.io.*;
 
 public class Servidor{
 
+	private static final int BUFFER_SIZE = 256;
+
 	private ServerInfo info;
 	private Queue<ServerInfo> servers;
+	private MulticastSocket socket;
+	private InetAddress group;
+	private int puertoDNS;
+	private String dirDNS;
+
+	public Servidor(String n, int gPort, String g, int dPort, String d){
+		servers = new LinkedList<ServerInfo>();
+		info = new ServerInfo(n,"Activo");
+		puertoDNS = dPort;
+		dirDNS = d;
+
+		try{
+			socket = new MulticastSocket(gPort);
+			group = InetAddress.getByName(g);
+			socket.joinGroup(group);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("Starting the send");
+		byte[] buf = new byte[BUFFER_SIZE];
+		String i = "SERVER";
+		buf = i.getBytes();
+
+		try{
+			DatagramPacket init = new
+				DatagramPacket(buf,buf.length,InetAddress.getByName(dirDNS),puertoDNS);
+			socket.send(init);
+
+			System.out.println("Sent");
+			buf = new byte[BUFFER_SIZE];
+			DatagramPacket rec = new DatagramPacket(buf,buf.length);
+			socket.receive(rec);
+			i = new String(rec.getData(),0,rec.getLength());
+			System.out.println("Received " + i);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String args[]){
+		Servidor s = new Servidor("Name",2222,"224.0.0.1",1111,"localhost");
+	}
 
 }
