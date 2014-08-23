921823b73f8abea22eadc119e4252ad7d0709195
Jose Montenegro <josmontenegro2012@gmail.com>
2014-08-23 14:18:00 -0430

    Arreglados tabs en Colas.java. Agregado documento de protocolo de mensajeria

--- Proyecto02/src/Colas.java
+++ Proyecto02/src/Colas.java
@@ -13,56 +13,52 @@ import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Queue;
 
-public class Colas<E>
-{
+public class Colas<E>{
 	
-    Queue<E> cola;
-    int elem;
+	Queue<E> cola;
+	int elem;
     	
-    /**
-     * Constructor de Colas.
-     */
-    public Colas() {
-	cola = new LinkedList<E>();
-	elem = 0;
-    }
+	/**
+	* Constructor de Colas.
+	*/
+	public Colas() {
+		cola = new LinkedList<E>();
+		elem = 0;
+	}
 
-    public synchronized Queue<E> getQueue() {
-    	return cola;
-    }
+	public synchronized Queue<E> getQueue() {
+		return cola;
+	}
 	
-    public synchronized void addElem(E p){
-	cola.add(p);
-	if(this.elem == 0)
-	    notifyAll();
-	this.elem++;
-	
-    } 
+	public synchronized void addElem(E p){
+		cola.add(p);
+		this.elem++;
+		if(this.elem == 1)
+			notifyAll();
+	} 
     
-    public synchronized E removeElem() {
-	if(this.elem == 0)
-	    try{
-		wait();
-	    }catch(IllegalMonitorStateException | InterruptedException e){
-		e.printStackTrace();
-		System.exit(-1);
-	    }
-	this.elem--;
-	return cola.remove();
-	
-    }
+	public synchronized E removeElem() {
+		if(this.elem == 0)
+			try{
+				wait();
+			}catch(IllegalMonitorStateException | InterruptedException e){
+				e.printStackTrace();
+				System.exit(-1);
+		}
+		this.elem--;
+		return cola.remove();
+	}
     
-    public synchronized E peekElem(){
-	return cola.peek(); //Nuevo
-    }
+	public synchronized E peekElem(){
+		return cola.peek();
+	}
 
-    public synchronized boolean isEmpty(){
-	return cola.isEmpty();
-    }
+	public synchronized boolean isEmpty(){
+		return cola.isEmpty();
+	}
 
-    public synchronized int size(){
-    	return cola.size();
-    }
+	public synchronized int size(){
+		return cola.size();
+	}
         
-    
 }
