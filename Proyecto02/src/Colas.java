530523bff7f1f8522974a58bc127e170c48d0de8
Luis Miranda <lcmm1993@gmail.com>
2014-08-27 01:15:00 -0430

    Prueba de identacion

--- Proyecto02/src/Colas.java
+++ Proyecto02/src/Colas.java
@@ -15,54 +15,57 @@ import java.util.Queue;
 
 public class Colas<E>{
 	
-	Queue<E> cola;
-	int elem;
+    Queue<E> cola;
+    int elem;
     	
-	/**
-	* Constructor de Colas.
-	*/
-	public Colas() {
-		cola = new LinkedList<E>();
-		elem = 0;
-	}
+    /**
+     * Constructor de Colas.
+     */
+    public Colas() {
+        cola = new LinkedList<E>();
+        elem = 0;
+    }
 
-	public synchronized Queue<E> getQueue() {
-		return cola;
-	}
+    public synchronized Queue<E> getQueue() {
+        return cola;
+    }
 
     public synchronized void setQueue(Queue<E> s) {
 	this.cola = s;
     }
 	
-	public synchronized void addElem(E p){
-		cola.add(p);
-		this.elem++;
-		if(this.elem == 1)
-			notifyAll();
-	} 
+    public synchronized void addElem(E p){
+        cola.add(p);
+        this.elem++;
+        if(this.elem == 1)
+            notifyAll();
+    } 
     
-	public synchronized E removeElem() {
-		if(this.elem == 0)
-			try{
-				wait();
-			}catch(IllegalMonitorStateException | InterruptedException e){
-				e.printStackTrace();
-				System.exit(-1);
-		}
-		this.elem--;
-		return cola.remove();
-	}
+    public synchronized E removeElem() {
+        if(this.elem == 0)
+            try{
+                wait();
+            }catch(IllegalMonitorStateException | InterruptedException e){
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        this.elem--;
+        return cola.remove();
+    }
+    public synchronized void removeElem(E o) {
+	this.cola.remove(o);
+    }
     
-	public synchronized E peekElem(){
-		return cola.peek();
-	}
+    public synchronized E peekElem(){
+        return cola.peek();
+    }
 
-	public synchronized boolean isEmpty(){
-		return cola.isEmpty();
-	}
+    public synchronized boolean isEmpty(){
+        return cola.isEmpty();
+    }
 
-	public synchronized int size(){
-		return cola.size();
-	}
+    public synchronized int size(){
+        return cola.size();
+    }
         
 }
