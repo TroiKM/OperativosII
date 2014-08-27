6adcb97d8ea9071ae902433a1fc068c50eca3357
Luis Miranda <lcmm1993@gmail.com>
2014-08-26 23:22:20 -0430

    Ya todos se mandan mensajes bonitos

--- Proyecto02/src/Trabajador.java
+++ Proyecto02/src/Trabajador.java
@@ -8,82 +8,93 @@ import java.util.Queue;
 
 public class Trabajador implements Runnable{
 
-	private Colas<DatagramPacket> mensajes;
-	private MulticastSocket socket;
+        private Colas<DatagramPacket> mensajes;
+        private MulticastSocket socket;
     
-	private InetAddress group;
-	private int puertoDNS;
-	private InetAddress dirDNS;
-	private int time;
+        private InetAddress group;
+        private int puertoDNS;
+        private InetAddress dirDNS;
+        private int time;
 
-	private Servicios serv;
-	private int puerto;
-	private ServerInfo info;
+        private Servicios serv;
+        private int puerto;
+        private ServerInfo info;
     private Colas<ServerInfo> servers;
     
 
     public Trabajador(Colas<DatagramPacket> c,MulticastSocket s, int p, ServerInfo i,Colas<ServerInfo> ser){
-		mensajes = c;
-		socket = s;
-		time = 0;
-		puerto = p;
-		info = i;
-		servers = ser;
-	}
+                mensajes = c;
+                socket = s;
+                time = 0;
+                puerto = p;
+                info = i;
+                servers = ser;
+        }
 
-	public void run(){
-	
-		while(true){
-		
-			Mensaje men = null;
-			time++;
-			DatagramPacket wrd = this.mensajes.removeElem();
-			
-			if(wrd!=null){
-		
-				try{
-					men = Mensajeria.decodePacket(wrd);
-					time = Math.max(men.getTime(),time);
-					time++;
-					executeCommand(men);
-				}catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+        public void run(){
+        
+                while(true){
+                
+                        Mensaje men = null;
+                        time++;
+                        DatagramPacket wrd = this.mensajes.removeElem();
+                        
+                        if(wrd!=null){
+                
+                                try{
+                                        men = Mensajeria.decodePacket(wrd);
+                                        time = Math.max(men.getTime(),time);
+                                        time++;
+                                        executeCommand(men);
+                                }catch(IOException e){
+                                        e.printStackTrace();
+                                }
+                        }
+                }
+        }
 
-	private void executeCommand(Mensaje men){
-		String com = men.getCommand();
+        private void executeCommand(Mensaje men){
+                String com = men.getCommand();
 
-		if(com.equals("COORD")){
-			try{
-				serv = new ServiciosImpl("localhost",String.valueOf(puerto));
-				LocateRegistry.createRegistry(puerto);
-				Naming.rebind("rmi://localhost:" + puerto + "/Servicios",serv);
-				this.servers.addElem(this.info);
+                if(com.equals("COORD")){
+                        try{
+                                serv = new ServiciosImpl("localhost",String.valueOf(puerto));
+                                LocateRegistry.createRegistry(puerto);
+                                Naming.rebind("rmi://localhost:" + puerto + "/Servicios",serv);
+//                              this.servers.addElem(this.info);
 
-			}catch(RemoteException e){
-				e.printStackTrace();
-			}catch(MalformedURLException e){
-				e.printStackTrace();
-			}
-			System.out.println("Ahora soy el cordi");
-		}else if ( com.equals("OK") ) {
-			try{
-				Mensajeria.sendMessage(this.socket,(InetAddress)men.getAttribute(0),(int)men.getAttribute(1),"SERVER",time, this.info );
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-		}else if (com.equals("SERVER")) {
-			ServerInfo i = (ServerInfo) men.getAttribute(0);
-			this.servers.addElem(i);
-			Mensajeria.broadcast(this.socket,this.servers.getQueue(),"NEWSERVER",time, this.servers.getQueue());
-		} else if (com.equals("NEWSERVER")){
-		    this.servers.setQueue((Queue<ServerInfo>)men.getAttribute(0));
-		    System.out.println(this.servers.getQueue());
-		}
+                        }catch(RemoteException e){
+                                e.printStackTrace();
+                        }catch(MalformedURLException e){
+                                e.printStackTrace();
+                        }
+                        System.out.println("Ahora soy el cordi");
+                }else if ( com.equals("OK") ) {
+                        try{
+                                Mensajeria.sendMessage(this.socket,(InetAddress)men.getAttribute(0),(int)men.getAttribute(1),"SERVER",time, this.info );
+                        }catch(IOException e){
+                                e.printStackTrace();
+                        }
+                }else if (com.equals("SERVER")) {
+                    ServerInfo i = (ServerInfo) men.getAttribute(0);
+                    Queue<ServerInfo> dummy = new LinkedList<ServerInfo>(this.servers.getQueue());
+                    dummy.add(this.info);
 
-	}
+                    try{
+                        Mensajeria.sendMessage(this.socket,i.getIP(),i.getPuerto(),"NEWSERVER",time, dummy);
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                    
+                    dummy.add(i);
+                    Mensajeria.broadcast(this.socket,this.servers.getQueue(),"NEWSERVER",time, dummy);
+                    this.servers.addElem(i);
+
+                } else if (com.equals("NEWSERVER")){
+                    this.servers.setQueue((Queue<ServerInfo>)men.getAttribute(0));
+                    System.out.println(this.servers.getQueue());
+                }
+
+        }
 
 } 
